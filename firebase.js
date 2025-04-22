576f20eee308ff4b58675e65b130ec394215e905
editincyt <editincyt@gmail.com>
2025-04-23 01:10:36 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,11 +1,18 @@
-import { initializeApp, applicationDefault } from 'firebase-admin/app';
-import { getDatabase } from 'firebase-admin/database';
+import { initializeApp } from 'firebase/app';
+import { getDatabase } from 'firebase/database';
 
-initializeApp({
-  credential: applicationDefault(),
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
   databaseURL: process.env.FIREBASE_DATABASE_URL,
-});
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID
+};
 
-const db = getDatabase();
+// Firebase'i başlat
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
 
 export { db };
