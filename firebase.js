468049b900a01c2548e25bf7ca996f942a96ee1a
editincyt <editincyt@gmail.com>
2025-04-23 01:18:40 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,18 +1,23 @@
-import { initializeApp } from 'firebase/app';
-import { getDatabase } from 'firebase/database';
+// firebase.js
+import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
 
+// Firebase yapılandırma bilgileri
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.FIREBASE_DATABASE_URL,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID
+  apiKey: "API_KEY",  // Firebase API Key
+  authDomain: "plugain-1f481.firebaseapp.com",
+  databaseURL: "https://plugain-1f481-default-rtdb.europe-west1.firebasedatabase.app/",
+  projectId: "plugain-1f481",
+  storageBucket: "plugain-1f481.appspot.com",
+  messagingSenderId: "SENDER_ID",
+  appId: "APP_ID",
+  measurementId: "G-MEASUREMENT_ID"
 };
 
-// Firebase'i başlat
+// Firebase uygulamasını başlat
 const app = initializeApp(firebaseConfig);
+
+// Firebase Realtime Database bağlantısını al
 const db = getDatabase(app);
 
 export { db };
