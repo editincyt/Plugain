ed530ec68930a457ae386c339aac03013f6da929
editincyt <editincyt@gmail.com>
2025-04-23 03:07:34 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,9 +1,3 @@
 export const firebaseConfig = {
   databaseURL: "https://plugain-1f481-default-rtdb.europe-west1.firebasedatabase.app/"
-  function toggleDarkMode() {
-  document.body.classList.toggle('dark');
-  const mode = document.body.classList.contains('dark') ? '🌞 Light Mode' : '🌙 Dark Mode';
-  document.querySelector('.toggle-mode').textContent = mode;
-}
-
 };
